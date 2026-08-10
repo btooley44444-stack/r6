4346197ea46319390eeb51f64835fe8fae71874d
btooley44444-stack <btooley44444@gmail.com>
2026-08-10 14:43:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -700,7 +700,7 @@ async function autoRestore(guild) {
 client.once('ready', async () => {
   console.log(`✅ ${client.user.tag} online`);
   client.user.setPresence({
-    activities: [{ name: '.gg/rasengan', type: ActivityType.Watching }],
+    activities: [{ name: 'BREN THE GOAT', type: ActivityType.Watching }],
     status: 'online',
   });
 
